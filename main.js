17d9833a766e779b127ed451969b352c8ed3a891
AnonymousParadox890 <abhyudaya890@gmail.com>
2021-12-20 19:34:59 +0530

    *

---
+++ main.js
@@ -0,0 +1,8 @@
+const showMsg = () => {
+  Swal.fire({
+  confirmButtonText: '<img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-ghost-halloween-justicon-flat-justicon-2.png"/>',
+  title: '<img src="https://img.icons8.com/external-justicon-lineal-color-justicon/64/000000/external-ghost-halloween-justicon-lineal-color-justicon-2.png"/>',
+  background:'#42445A'
+});
+
+}
